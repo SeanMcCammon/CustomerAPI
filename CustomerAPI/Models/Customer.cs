e5ce0cb9c930dd842fd61cf36cc6de5e4aa54bdf
Sean McCammon <sean_mccammon@hotmail.com>
2020-04-21 22:08:00 +0100

    Added minimum and maximum character validation on first and surname to ensure between 3 and 50 characters in length

--- CustomerAPI/Models/Customer.cs
+++ CustomerAPI/Models/Customer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
 
 namespace CustomerAPI.Models
 {
@@ -11,8 +12,10 @@ namespace CustomerAPI.Models
         [Key]
         public long Id { get; set; }
         [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "The Surname is required to be between 3 - 50 characters in length . ")]
         public string Surname { get; set; }
         [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "The First Name is required to be between 3 - 50 characters in length. ")]
         public string FirstName { get; set; }
         public string Policy { get; set; }
         [EmailAddress]
