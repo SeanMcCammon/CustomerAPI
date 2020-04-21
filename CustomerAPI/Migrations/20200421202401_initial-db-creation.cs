d98a9e598ecee5a4b82c83e27cde9c8f85037ad2
Sean McCammon <sean_mccammon@hotmail.com>
2020-04-21 21:45:30 +0100

    Add project files.

---
+++ CustomerAPI/Migrations/20200421202401_initial-db-creation.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CustomerAPI.Migrations
+{
+    public partial class initialdbcreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Surname = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(nullable: false),
+                    Policy = table.Column<string>(nullable: true),
+                    EMail = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
---
+++ CustomerAPI/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerAPI.Models
+{
+    public class Customer
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        public string Policy { get; set; }
+        [EmailAddress]
+        public string EMail { get; set; }
+
+        public DateTime DOB { get; set; }
+    }
+}
